02fc7b846722580ee8f5e9de4fa1e3b81902972c
nrs365 <akakitaiga@gmail.com>
2014-05-09 14:05:41 -0500

    testingpush

--- if_and_or.php
+++ if_and_or.php
@@ -1,33 +1,13 @@
 <?php
-$x = 0;
-$y = 5;
-$z = 10;
 
- if (($x < $y) && ($y < $z)) {
- 	echo "{$x} < {$y} < {$z}\n";
- }	
+$a = 10;
+$b = '10';
 
- if (($x > 0) || ($x < 10)){
- 	 echo "$x is greater than 0 OR less than 10\n";
- 	} 
-
- if (($y > 0) || ($x < 10)){
- 	echo "$y is greater than 0 Or less than 10\n";
- }
-
- if (($z > 10) || ($x < 10)) {
- 	echo "$z is greter than 0 or less than 10\n";
- }
-
- if (($x > 0) && ($x < 10)) {
- 	echo "$x is greater than 0 AND less than 10\n";
- }
-
- if (($y > 0) && ($y < 10)) {
- 	echo "$y is greater than 0 and less than 10\n";
- }
-
- if (($z > 0) && ($z < 10)) {
- 	echo "$z is greater than 0 and less than 10\n";
- }
-
+if ($a === $b) {
+    echo "$a is identical to $b";
+} elseif ($a == $b) {
+    echo "$a is equal to $b";
+} else {
+    echo "$a is not equal to $b";
+}
+//adding testing
