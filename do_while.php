e71c584e5a114a5d15bf8be2547a480fedf17b38
nrs365 <nrs365@gmail.com>
2014-05-12 08:50:06 -0500

    adding multiplying by

--- do_while.php
+++ do_while.php
@@ -10,8 +10,17 @@
 
 
 // Alter your loop to count backwards by 5's from 100 to -10.
-$a = 100;
+// $a = 100;
+// do {
+// 	echo "$a\n";
+// 	$a -= 5;
+// } while ($a >= -10);
+
+//Create a do-while loop that starts at 2, and displays the result $a * $a on each line while $a is less than 1,000,000. Output should equal:
+// 2, 4, 16, 256, 65536
+$a = 2;
 do {
 	echo "$a\n";
-	$a -= 5;
-} while ($a >= -10);
+	$a *= $a;
+
+} while ($a < 1000000);
