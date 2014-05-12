8ab68199a5d26cb870a069548d6c5f7c69307432
nrs365 <nrs365@gmail.com>
2014-05-12 16:42:01 -0500

    added increment, changed input style

--- for.php
+++ for.php
@@ -1,14 +1,24 @@
 <?php
 
-fwrite(STDOUT, "Please input two numbers for range ");
-if(!is_numeric($argv[1]) || !is_numeric($argv[2])){
-	fwrite(STDOUT, "Please input numbers only ");
-}
+// fwrite(STDOUT, "Please input two numbers for range and one number as an increment ");
+// if(!is_numeric($argv[1]) || !is_numeric($argv[2])|| !is_numeric($argv[3])){
+// 	fwrite(STDOUT, "Please input numbers only ");
+// }
+fwrite(STDOUT, "Please input a number for beginning range ");
+$min = fgets(STDIN);
 
-if ($argc == 3){
-	$min = $argv[1];
-	$max = $argv[2];
-}
-for ($i = $min; $i <= $max; $i++) {
+fwrite(STDOUT, "Please input a number for a maximum range ");
+$max = fgets(STDIN);
+
+fwrite(STDOUT, "Please input a number for an increment ");
+$increment = fgets(STDIN);
+
+
+// if ($argc == 4){
+// 	$min = $argv[1];
+// 	$max = $argv[2];
+// 	$increment = $argv[3];
+// }
+for ($i = $min; $i <= $max; $i += $increment) {
 	echo "$i\n";
 }
