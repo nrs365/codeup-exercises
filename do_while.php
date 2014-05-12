5b1a6d1a01b61f3cf79498b2d584aa52548bed94
nrs365 <nrs365@gmail.com>
2014-05-12 08:42:24 -0500

    counting backwards from 100 - -10

--- do_while.php
+++ do_while.php
@@ -1,9 +1,17 @@
 <?php
 
-$a = 0;
+// Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a newline.
+// // $a = 0;
 
+// // do {
+// // 	echo "$a\n";
+// // 	$a += 2;
+// // } while ($a <= 100);
+
+
+// Alter your loop to count backwards by 5's from 100 to -10.
+$a = 100;
 do {
 	echo "$a\n";
-	$a += 2;
-} while ($a <= 100);
-
+	$a -= 5;
+} while ($a >= -10);
