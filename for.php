0bc34932fa3d19e345a60c3a9a76785d5e596fde
nrs365 <nrs365@gmail.com>
2014-05-13 10:19:47 -0500

    added check for no input on incrementer and set default to 1

--- for.php
+++ for.php
@@ -2,24 +2,25 @@
 
 // fwrite(STDOUT, "Please input two numbers for range and one number as an increment ");
 // if(!is_numeric($argv[1]) || !is_numeric($argv[2])|| !is_numeric($argv[3])){
- // 	fwrite(STDOUT, "Please input numbers only ");
-//
+//  	fwrite(STDOUT, "Please input numbers only ");
+
 fwrite(STDOUT, "Please input a number for beginning range ");
-$min = trim(fgets(STDIN));
+$min = is_numeric(trim(fgets(STDIN)));
 
 fwrite(STDOUT, "Please input a number for a maximum range ");
-$max = trim(fgets(STDIN));
+$max = is_numeric(trim(fgets(STDIN)));
 
 fwrite(STDOUT, "Please input a number for an increment ");
-$increment = trim(fgets(STDIN));
+$increment = is_numeric(trim(fgets(STDIN)));
 	if (empty($increment)){
 		$increment = 1;
 	}
+if (!is_numeric($min) && !is_numeric($max) && !is_numeric($increment))
 
-// // if ($argc == 4){
-// // 	$min = $argv[1];
-// // 	$max = $argv[2];
-// // 	$increment = $argv[3];
+// if ($argc == 4){
+// 	$min = $argv[1];
+// 	$max = $argv[2];
+// 	$increment = $argv[3];
 // } else if ($argc == 3) {
 // 	$min = $argv[1];
 // 	$max = $argv[2];
