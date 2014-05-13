779e5156e217722899abb7b8462e770f417ff71a
nrs365 <nrs365@gmail.com>
2014-05-13 17:17:50 -0500

    refactored code to use a function to decrease repetitiveness

--- for.php
+++ for.php
@@ -1,49 +1,27 @@
 <?php
-
-// fwrite(STDOUT, "Please input two numbers for range and one number as an increment ");
-// if(!is_numeric($argv[1]) || !is_numeric($argv[2])|| !is_numeric($argv[3])){
-//  	fwrite(STDOUT, "Please input numbers only ");
+function is_input_numeric($input){
+	while(!is_numeric($input)){
+		fwrite(STDOUT, "Please input only numbers\n");
+		$input = (trim(fgets(STDIN)));
+	}
+	return $input; // this sets the variable when it exits the loop
+}
 
 fwrite(STDOUT, "Please input a number for beginning range ");
 $min = trim(fgets(STDIN));
-	if (!is_numeric($min)) {
-		do {
-		fwrite(STDOUT, "Please input only numbers\n");
-		$min = (trim(fgets(STDIN)));
-		} while (!is_numeric($min));
-	}	
-
+$min = is_input_numeric($min);
 
 fwrite(STDOUT, "Please input a number for a maximum range ");
 $max = trim(fgets(STDIN));
-	if (!is_numeric($max)) {
-		do {
-		fwrite(STDOUT, "Please input only numbers\n");
-		$max = (trim(fgets(STDIN)));
-		} while (!is_numeric($max));
-	}	
+$max = is_input_numeric($max);
+
 fwrite(STDOUT, "Please input a number for an increment ");
 $increment = trim(fgets(STDIN));
 	if (empty($increment)){
 		$increment = 1;
-	}else if (!is_numeric($increment)) {
-		do {
-		fwrite(STDOUT, "Please input only numbers\n");
-		$increment = (trim(fgets(STDIN)));
-		} while (!is_numeric($increment));
-	}	
-	
-
-
-// if ($argc == 4){
-// 	$min = $argv[1];
-// 	$max = $argv[2];
-// 	$increment = $argv[3];
-// } else if ($argc == 3) {
-// 	$min = $argv[1];
-// 	$max = $argv[2];
-// 	$increment = 1;
-// }
+	} else {
+		is_input_numeric($increment);
+	}
 	
 for ($i = $min; $i <= $max; $i += $increment) {
 	echo "$i\n";
