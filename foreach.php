663eed4be6da4ff4126360e4f080bd88fb0faa86
nrs365 <nrs365@gmail.com>
2014-05-13 14:46:46 -0500

    echo out value in array

--- foreach.php
+++ foreach.php
@@ -1,24 +1,7 @@
 <?php
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
-// foreach ($things as $thing) {
-//     if (is_int($thing)) {
-//         echo "{$thing} is an integer.\n";
-//     } else if (is_string($thing)) {
-//         echo "{$thing} is a string.\n";
-//     } else if (is_float($thing)){
-//     	echo "{$thing} is a float.\n";
-//     } else if (is_bool($thing)){
-//     	echo "{$thing} is boolean.\n";
-//     } else if (is_array($thing)){
-//     	echo "{$thing} is an array.\n";
-//     } else if (is_null($thing)){
-//     	echo "{$thing} is NULL.\n";
-//     }
-// }
 
-foreach ($things as $thing){
-	if (is_scalar($thing)){
-		echo "{$thing} is scalar\n";
-	}
+foreach($things as $thing){
+	echo "{$thing}\n";
 }
