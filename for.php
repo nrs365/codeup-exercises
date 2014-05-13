e18c84aae1b1fe77c94234cdc7e1d9c6f05dd1f2
nrs365 <nrs365@gmail.com>
2014-05-13 10:04:57 -0500

    refactored to prompt user with fwrite/fgets and set default increment to 1 using empty

--- for.php
+++ for.php
@@ -1,28 +1,32 @@
 <?php
 
-fwrite(STDOUT, "Please input two numbers for range and one number as an increment ");
-if(!is_numeric($argv[1]) || !is_numeric($argv[2])|| !is_numeric($argv[3])){
-	fwrite(STDOUT, "Please input numbers only ");
-} 
-// fwrite(STDOUT, "Please input a number for beginning range ");
-// $min = fgets(STDIN);
+// fwrite(STDOUT, "Please input two numbers for range and one number as an increment ");
+// if(!is_numeric($argv[1]) || !is_numeric($argv[2])|| !is_numeric($argv[3])){
+ // 	fwrite(STDOUT, "Please input numbers only ");
+//
+fwrite(STDOUT, "Please input a number for beginning range ");
+$min = trim(fgets(STDIN));
 
-// fwrite(STDOUT, "Please input a number for a maximum range ");
-// $max = fgets(STDIN);
+fwrite(STDOUT, "Please input a number for a maximum range ");
+$max = trim(fgets(STDIN));
 
-// fwrite(STDOUT, "Please input a number for an increment ");
-// $increment = fgets(STDIN);
+fwrite(STDOUT, "Please input a number for an increment ");
+$increment = trim(fgets(STDIN));
+	if (empty($increment)){
+		$increment = 1;
+	}
 
-
-if ($argc == 4){
-	$min = $argv[1];
-	$max = $argv[2];
-	$increment = $argv[3];
-} else if ($argc == 3) {
-	$min = $argv[1];
-	$max = $argv[2];
-	$increment = 1;
-}
+// // if ($argc == 4){
+// // 	$min = $argv[1];
+// // 	$max = $argv[2];
+// // 	$increment = $argv[3];
+// } else if ($argc == 3) {
+// 	$min = $argv[1];
+// 	$max = $argv[2];
+// 	$increment = 1;
+// }
+	
 for ($i = $min; $i <= $max; $i += $increment) {
 	echo "$i\n";
 }
+
