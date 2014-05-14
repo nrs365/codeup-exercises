2cc94bd81ab7f8b5092b8077af3dc1452f5988ee
nrs365 <nrs365@gmail.com>
2014-05-14 09:54:02 -0500

    changed to output color of fruit

--- fruit.php
+++ fruit.php
@@ -5,7 +5,15 @@
 // comment out and cont - modify fruit array so that you have fruitname=>color;
 // three or more fruit; print usings foreach loop "Apples are red."
 
-$fruits = ["strawberry" => "ichigo", "peach" => "momo", "plum" => "ume"];
-foreach ($fruits as $key => $value){
-	echo $value . PHP_EOL;
+$fruits = ["Strawberries" => "red", "Peaches" => "peach", "Plums" => "purple"];
+
+foreach ($fruits as $fruit => $color){
+	echo "{$fruit} are {$color}.\n";
 }
+// $num_of_fruits = count($fruits);
+
+// for($i = 0; $i < $num_of_fruits; $i++){
+// 	echo $fruits[$i] . PHP_EOL;
+// }
+
+//for($i = 0; $i < count($fruits); $i++){}
