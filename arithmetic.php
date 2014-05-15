ad2605b7921bc0b7e9bc6099ee344119f5c50bf8
nrs365 <nrs365@gmail.com>
2014-05-15 09:44:17 -0500

    wrote functions for addition, subtraction, multiplication, and division

---
+++ arithmetic.php
@@ -0,0 +1,19 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
+}
+
+function subtract($a, $b) {
+    echo $a - $b . PHP_EOL;
+}
+
+function multiply($a, $b) {
+    echo $a * $b . PHP_EOL;
+}
+
+function divide($a, $b) {
+    echo $a / $b . PHP_EOL;
+}
+
+?>
