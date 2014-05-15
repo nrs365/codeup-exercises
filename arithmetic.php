6ff2aacb51ff6b9543645422feb337f999aea256
nrs365 <nrs365@gmail.com>
2014-05-15 11:40:26 -0500

    refactored code to check for numeric variables and display an error if the variable isn't numeric

--- arithmetic.php
+++ arithmetic.php
@@ -1,29 +1,90 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+		echo $a + $b;
+	} else {
+		echo 'ERROR: both $a and $b should be numbers';
+	}
+	echo PHP_EOL;
+
 }
 
 function subtract($a, $b) {
-    echo $a - $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+		echo $a - $b;
+	} else {
+		echo 'ERROR: both $a and $b should be numbers';
+	}
+	echo PHP_EOL;
 }
 
 function multiply($a, $b) {
-    echo $a * $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+		echo $a * $b;
+	} else {
+		echo 'ERROR: both $a and $b should be numbers';
+	}
+	echo PHP_EOL;
 }
 
 function divide($a, $b) {
-    echo $a / $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+		echo $a / $b;
+	} else {
+		echo 'ERROR: both $a and $b should be numbers';
+	}
+	echo PHP_EOL;
 }
 
 function remainder($a, $b) {
-	echo $a % $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+		echo $a % $b;
+	} else {
+		echo 'ERROR: both $a and $b should be numbers';
+	}
+	echo PHP_EOL;
 }
 
+// function compare($a, $b, $strict = true) { //$a mixed, $b mixed, $strict bool, set to default $strict = true)
+// 	//echo $a == $b ? "true" : "false";
+// 	//echo PHP_EOL;
+// 	if($strict === true) {
+// 		echo $a === $b ? 'true' : 'false';//strong comparison
+// 	} else {
+// 		echo $a == $b ? 'true' : 'false'; //weak comparison
+// 	}
+// 	echo PHP_EOL;
+// }
+
 add (100, 100);
-subtract(100, 50);
+subtract('sleepy', 50);
 multiply(10, 10);
 divide(100, 2);
 remainder(5,2)
+//compare(42, '42', true)
 
+// function inspect($variable = null, $dump = true){
+// 	if ($dump === true){
+// 		var_dump($variable);
+// 	} else {
+// 		print_r($variable);
+// 	}
+// }
+// //$heroes = ['batman', 'superman', 'fall out boy'];
+// //inspect($heroes);
+// inspect(['batman', 'superman', 'fall out boy']);
+
+// function adder($a, $b){
+// 	if (is_numeric($a) && is_numeric($b)){
+// 		echo $a + $b;
+// 	} else {
+// 		echo 'ERROR: both $a and $b should be numbers';
+// 	}
+// 	echo PHP_EOL;
+// }
+// adder(1, adder(1,2));
+// adder(pi(), pi())
+// adder (pi(), false);
+// adder("2+3", 3);//eror
 ?>
