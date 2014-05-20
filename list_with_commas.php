fff2b932b92a8e37d9d61cc69731507052159acd
nrs365 <nrs365@gmail.com>
2014-05-20 14:25:34 -0500

    added a second function to check for sorting alphabetically to make sorting optional

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,26 +1,34 @@
 <?php
 
 // Converts array into list n1, n2, ..., and n3
-function humanized_list($string) {
+function humanized_list($string, $sorting = false) {
   // Your solution goes here!
 		$array = explode(', ', $string);
-		natcasesort($array);
+		
+		if($sorting){
+			sorting_option($array);
+		}
+			
 		$last_item = array_pop($array);
-		$new_last_item = 'and '. $last_item . PHP_EOL;
+		$new_last_item = 'and '. $last_item;
 		array_push($array, $new_last_item);
 		$new_string = implode(', ', $array);
 		return $new_string;
 	
 }
 
+function sorting_option($array) {
+	return natcasesort($array);
+}
+
 // List of famous peeps
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
 // Humanize that list
-$famous_fake_physicists = humanized_list($physicists_string);
+$famous_fake_physicists = humanized_list($physicists_string, true);
 
 // Output sentence
-echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.\n";
 
 
 //take last item off array, change it to add and, then push it back on the array
