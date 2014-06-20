04d9cd9e6b3f89ac7400ec311fafa99b164486d7
nrs365 <nrs365@gmail.com>
2014-06-20 14:54:17 -0500

    added PDO script to connect to mysql

---
+++ php_pdo.php
@@ -0,0 +1,10 @@
+<?php
+
+// Get new instance of PDO object
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_test_db', 'nicole', 'bakagaki');
+
+// Tell PDO to throw exceptions on error
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+?>
